61f992958583d89b0ccfc77512764888b5064638
andreavio00 <andrea.vio@outlook.it>
2026-07-09 09:32:54 +0200

    Refactor fetch to show detailed weather data
    Updated fetch handling to display detailed weather information.

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 fetch(
   "https://dati.meteotrentino.it/service.asmx/datiRealtimeUnaStazione?stazione=T0403&h=1"
 )
-.then(r => r.json())
+.then(response => response.json())
 .then(data => {
-  console.log(data);
-  document.body.innerHTML =
-    "<pre>" +
-    JSON.stringify(data.features[0], null, 2) +
-    "</pre>";
+
+  const first = data.features[0].properties;
+
+  document.body.innerHTML = `
+    <h1>Test Meteo Trentino</h1>
+
+    <p><b>Stazione:</b> ${first.staz}</p>
+    <p><b>Quota:</b> ${first.quota}</p>
+    <p><b>Data:</b> ${first.datetime}</p>
+
+    <pre>${JSON.stringify(first, null, 2)}</pre>
+  `;
+
 })
-.catch(err => {
-  document.body.innerHTML =
-    "<h2>Errore</h2><pre>" + err + "</pre>";
+.catch(error => {
+
+  document.body.innerHTML = `
+    <h1>Errore</h1>
+    <pre>${error}</pre>
+  `;
+
+  console.error(error);
+
 });
