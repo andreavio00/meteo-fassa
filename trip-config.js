8a130bd4d172e3c49a30890937b0bd8ccf3b9f77
andreavio00 <andrea.vio@outlook.it>
2026-09-10 09:43:49 +0200

    Aggiunge pagina escursioni

---
+++ trip-config.js
@@ -0,0 +1,10 @@
+window.METEO_FASSA_TRIPS={
+ stationsUrl:"https://gite-meteo-aggregator.andrea-vio.workers.dev/",
+ forecastUrl:"https://gite-previsioni-aggregator.andrea-vio.workers.dev",
+ zones:{
+  catinaccio:{name:"Catinaccio",forecastId:"catinaccio",stations:["fassa:gardeccia","fassa:principe","trentino:costalunga"]},
+  sella:{name:"Sella e Sassolungo",forecastId:"sassolungo_sella",stations:["fassa:passosella","fassa:sasspordoi","fassa:pizboe","fassa:coldeirossi","predazzo:colrodella","trentino:campitello","trentino:gries"]},
+  marmolada:{name:"Marmolada e Val San Nicolò",forecastId:"marmolada_val_s_nicolo",stations:["trentino:fedaia","trentino:ciampac","trentino:sasdelmul","fassa:coldeirossi","fassa:sasspordoi","trentino:gries"]},
+  moena:{name:"Moena e Latemar",forecastId:"moena_latemar",stations:["trentino:moena","trentino:costalunga","fassa:rolle","fassa:paradiso","predazzo:gardone","predazzo:passofeudo","predazzo:torredipisa"]}
+ }
+};
